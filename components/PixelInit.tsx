10068df77b797cdc2d8db1c21aa4b1301c3e8dd5
hamburglar00 <hamburglar002@proton.me>
2026-03-22 14:28:09 -0300

    chore: push latest PixelInit update

--- components/PixelInit.tsx
+++ components/PixelInit.tsx
@@ -10,62 +10,56 @@ export default function PixelInit({ pixelId }: Props) {
   if (!pixelId) return null;
 
   return (
-    <>
-      <Script id={`meta-pixel-base-${pixelId}`} strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;
-          n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];
-          t=b.createElement(e);t.async=!0;t.src=v;
-          s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-        `}
-      </Script>
+    <Script id={`meta-pixel-${pixelId}`} strategy="afterInteractive">
+      {`
+        (function () {
+          try {
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];
+            t=b.createElement(e);t.async=!0;t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
 
-      <Script id={`meta-pixel-init-${pixelId}`} strategy="afterInteractive">
-        {`
-          (function () {
+            window.__metaPixelInitDone = window.__metaPixelInitDone || {};
+            if (window.__metaPixelInitDone['${pixelId}']) return;
+
+            var params = new URLSearchParams(window.location.search);
+            var email = (params.get('email') || '').trim().toLowerCase();
+            var phoneRaw = params.get('phone') || '';
+            var phone = phoneRaw.replace(/\\D+/g, '');
+            if (phone.length === 10) phone = '54' + phone;
+
+            var externalId = '';
             try {
-              window.__metaPixelInitDone = window.__metaPixelInitDone || {};
-              if (window.__metaPixelInitDone['${pixelId}']) return;
-
-              var params = new URLSearchParams(window.location.search);
-              var email = (params.get('email') || '').trim().toLowerCase();
-              var phoneRaw = params.get('phone') || '';
-              var phone = phoneRaw.replace(/\\D+/g, '');
-              if (phone.length === 10) phone = '54' + phone;
-
-              var externalId = '';
-              try {
-                externalId = localStorage.getItem('external_id') || '';
-                if (!externalId) {
-                  externalId =
-                    (window.crypto && typeof window.crypto.randomUUID === 'function')
-                      ? window.crypto.randomUUID()
-                      : (Date.now() + '-' + Math.random().toString(36).slice(2));
-                  localStorage.setItem('external_id', externalId);
-                }
-              } catch (e) {
-                externalId = Date.now() + '-' + Math.random().toString(36).slice(2);
+              externalId = localStorage.getItem('external_id') || '';
+              if (!externalId) {
+                externalId =
+                  (window.crypto && typeof window.crypto.randomUUID === 'function')
+                    ? window.crypto.randomUUID()
+                    : (Date.now() + '-' + Math.random().toString(36).slice(2));
+                localStorage.setItem('external_id', externalId);
               }
-
-              var advancedMatching = { external_id: externalId };
-              if (email) advancedMatching.em = email;
-              if (phone) advancedMatching.ph = phone;
-
-              fbq('init', '${pixelId}', advancedMatching);
-              fbq('track', 'PageView');
-
-              window.__metaPixelInitDone['${pixelId}'] = true;
             } catch (e) {
-              console.error('Meta Pixel init error', e);
+              externalId = Date.now() + '-' + Math.random().toString(36).slice(2);
             }
-          })();
-        `}
-      </Script>
-    </>
+
+            var advancedMatching = { external_id: externalId };
+            if (email) advancedMatching.em = email;
+            if (phone) advancedMatching.ph = phone;
+
+            fbq('init', '${pixelId}', advancedMatching);
+            fbq('track', 'PageView');
+
+            window.__metaPixelInitDone['${pixelId}'] = true;
+          } catch (e) {
+            console.error('Meta Pixel init error', e);
+          }
+        })();
+      `}
+    </Script>
   );
 }
